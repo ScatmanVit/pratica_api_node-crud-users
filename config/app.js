51ef68f508604c18076c8f4dbaa3b9ce0b13b456
Vitor Ribeiro Baradel <victorribeirobaradel@gmail.com>
2025-07-17 14:28:05 -0300

    criar a rota autenticada com as rotas privadas para o user

--- config/app.js
+++ config/app.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import userPublicRoutes from '../routes/public/user.public.js'
-import userPrivateRoutes from '../routes/private/admin.private.js'
+import userPrivateRoutes from '../routes/private/user.private.js'
+import AdminPrivateRoutes from '../routes/private/admin.private.js'
 import auth from '../middlwares/auth.middlwares.js'
 import isAdmin from '../middlwares/validation.middlewares.js'
 
@@ -10,7 +11,8 @@ App.use(cors())
 App.use(express.json())
 
 App.use('/', userPublicRoutes)
-App.use('/', auth, isAdmin, userPrivateRoutes)
-
+App.use('/', auth, isAdmin, AdminPrivateRoutes)
+/* repetir a linha de cima para as rotas privadas
+do usuário, só usar ao UserPrivateRoutes*/
 
 export default App
