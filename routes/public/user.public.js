fd078b83d3d2515b11970521a96a401a363d2da9
Vitor Ribeiro Baradel <victorribeirobaradel@gmail.com>
2025-07-17 14:31:43 -0300

    após a criação das telas no front, voltar para cá!!

--- routes/public/user.public.js
+++ routes/public/user.public.js
@@ -2,7 +2,7 @@ import Router from 'express'
 const router = Router()
 import userControllers from '../../controllers/user.controllers.js'
 
-
+ 
 router.post('/register', userControllers.createUserController)
 router.post('/login', userControllers.loginUserController)
 
