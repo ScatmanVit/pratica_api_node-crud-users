67bfa56ecf3a4c03314d4a294ef7080c2c26593c
Vitor Ribeiro Baradel <victorribeirobaradel@gmail.com>
2025-07-17 14:30:53 -0300

    aqui nas rotas, criar essas rotas do profile para cada dado: put, delete(user), profile, logout(também pro adm depois)

---
+++ routes/private/user.private.js
@@ -0,0 +1,5 @@
+/* criar a separação de controller para fazer
+   a utilização desse arquivo, porque no momento
+   os controller privados(que será criados) para o user
+   não tem um arquivo próprio
+*/
